66aaa7c96c9a9d38e3307f4748ddb8bf5d3bf792
ruben <s236819@studenti.polito.it>
2021-03-14 21:08:56 +0100

    First layout for rolling 2 dice

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.firstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello Android"
+        textView.gravity = Gravity.CENTER
         setContentView(R.layout.activity_main)
     }
 }
